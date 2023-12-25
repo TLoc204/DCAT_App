c184fb2c5038b3245740960b528e7ca5f5756624
thanh9460413 <hoangthanh9460413@gmail.com>
2023-12-25 16:11:59 +0700

    update

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -20,6 +20,7 @@ export default function Login() {
     const SignIn = () => {
         navigation.navigate('SignIn');
     };
+    
     const handleSubmit = async () => {
         setLoading(true);
         try {
