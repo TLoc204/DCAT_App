a67d2e3b5140994b7f9451825f81ab1c351a1a99
thanh9460413 <hoangthanh9460413@gmail.com>
2023-12-25 16:05:17 +0700

    update

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './components/Login/Login';
+import SignIn from './components/SignIn/SignIn';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+        {/* <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Icon
+                  name={'arrow-back-ios'}
+                  size={24}
+                  color="#667080"
+                  style={{
+                    marginLeft: 16,
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+            headerTitle:""
+
+          })}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ FirebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDdpAGLqYANPXgNt2R18eWeGw0udYWv4uM",
+    authDomain: "dcat-c09a4.firebaseapp.com",
+    databaseURL: "https://dcat-c09a4-default-rtdb.firebaseio.com",
+    projectId: "dcat-c09a4",
+    storageBucket: "dcat-c09a4.appspot.com",
+    messagingSenderId: "729078324594",
+    appId: "1:729078324594:web:8daf55a40b06bba5d691eb",
+    measurementId: "G-W5S2ZHGBDR"
+};
+export const FIREBASE_APP = initializeApp(firebaseConfig);
