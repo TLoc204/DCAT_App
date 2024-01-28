c0377cc214c22353f5473ae4eee0f282c6808f87
Truong Phi Tan Loc <96646825+TLoc204@users.noreply.github.com>
2024-01-28 22:28:44 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './components/Login/Login';
 import BottomBarNavigation from './components/BottomBarNavigation';
 import OrderScreen from './components/Order/OrderScreen';
-
+import { ImageAllFolderProvider } from './components/Order/FoodOrder'; // Import context
 const Stack = createNativeStackNavigator();
-
 export default function App() {
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="BottomBarNavigation" component={BottomBarNavigation} options={{ headerShown: false }} />
-        <Stack.Screen name="OrderScreen" component={OrderScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ImageAllFolderProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="BottomBarNavigation" component={BottomBarNavigation} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ImageAllFolderProvider>
   );
 }
