103aa3f4a5d1a915bdc8da1387aae20c635075f1
loctpt <vnam041@gmail.com>
2024-06-02 18:29:54 +0700

    y

--- components/Admin/AdminCreateAndUpdateFood.js
+++ components/Admin/AdminCreateAndUpdateFood.js
@@ -246,113 +246,113 @@ export default function AdminCreateAndUpdateFood({ route }) {
             }
             else {
                 if (photo) {
-                    deleteItemFromFirebase(key,imageName)
-                    const itemTypePrefix = key.match(/[A-Za-z]+/)[0];
-                    let uriParts = photo.assets[0]?.uri.split('/');
-                    let nameImage = uriParts[uriParts.length - 1];
-                    let response = '';
-                    let blob = '';
+                    // deleteItemFromFirebase(key,imageName)
+                    // const itemTypePrefix = key.match(/[A-Za-z]+/)[0];
+                    // let uriParts = photo.assets[0]?.uri.split('/');
+                    // let nameImage = uriParts[uriParts.length - 1];
+                    // let response = '';
+                    // let blob = '';
 
-                    if (photo) {
-                        try {
-                            uriParts = photo.assets[0]?.uri.split('/');
-                            nameImage = uriParts[uriParts.length - 1];
-                            response = await fetch(photo.assets[0]?.uri);
+                    // if (photo) {
+                    //     try {
+                    //         uriParts = photo.assets[0]?.uri.split('/');
+                    //         nameImage = uriParts[uriParts.length - 1];
+                    //         response = await fetch(photo.assets[0]?.uri);
 
-                            if (response.ok) {
-                                blob = await response.blob();
-                            } else {
-                                console.error('Error fetching the photo URI:', response.statusText);
-                                return;
-                            }
-                        } catch (error) {
-                            console.error('Error fetching the photo URI:', error);
-                            return;
-                        }
-                    }
+                    //         if (response.ok) {
+                    //             blob = await response.blob();
+                    //         } else {
+                    //             console.error('Error fetching the photo URI:', response.statusText);
+                    //             return;
+                    //         }
+                    //     } catch (error) {
+                    //         console.error('Error fetching the photo URI:', error);
+                    //         return;
+                    //     }
+                    // }
 
-                    let itemType = '';
-                    switch (itemTypePrefix) {
-                        case 'D':
-                            itemType = 'Drinks';
-                            break;
-                        case 'F':
-                            itemType = 'Foods';
-                            break;
-                        case 'DD':
-                            itemType = 'Drink2ND';
-                            break;
-                        case 'Tp':
-                            itemType = 'Topping';
-                            break;
-                        case 'Fb':
-                            itemType = 'FoodBonus';
-                            break;
-                        case 'G':
-                            itemType = 'Games';
-                            break;
-                        default:
-                            console.error("Unknown item type");
-                            return;
-                    }
+                    // let itemType = '';
+                    // switch (itemTypePrefix) {
+                    //     case 'D':
+                    //         itemType = 'Drinks';
+                    //         break;
+                    //     case 'F':
+                    //         itemType = 'Foods';
+                    //         break;
+                    //     case 'DD':
+                    //         itemType = 'Drink2ND';
+                    //         break;
+                    //     case 'Tp':
+                    //         itemType = 'Topping';
+                    //         break;
+                    //     case 'Fb':
+                    //         itemType = 'FoodBonus';
+                    //         break;
+                    //     case 'G':
+                    //         itemType = 'Games';
+                    //         break;
+                    //     default:
+                    //         console.error("Unknown item type");
+                    //         return;
+                    // }
 
-                    const Ref = ref(database, `${itemType}/${key}`);
-                    const storageReference = storageRef(storage, `${itemType}/${nameImage}`);
+                    // const Ref = ref(database, `${itemType}/${key}`);
+                    // const storageReference = storageRef(storage, `${itemType}/${nameImage}`);
 
-                    if (blob) {
-                        try {
-                            const uploadTask = uploadBytesResumable(storageReference, blob);
-                            uploadTask.on('state_changed',
-                                (snapshot) => {
-                                    // Observe state change events such as progress, pause, and resume
-                                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                                    console.log('Upload is ' + progress + '% done');
-                                    switch (snapshot.state) {
-                                        case 'paused':
-                                            console.log('Upload is paused');
-                                            break;
-                                        case 'running':
-                                            console.log('Upload is running');
-                                            break;
-                                    }
-                                },
-                                (error) => {
-                                    // Handle unsuccessful uploads
-                                    console.error('Error uploading image:', error);
-                                },
-                                () => {
-                                    // Handle successful uploads on complete
-                                    console.log('Image uploaded successfully');
-                                }
-                            );
-                        } catch (error) {
-                            console.error('Error uploading image:', error);
-                            return;
-                        }
-                    } else {
-                        console.error('No blob available for upload');
-                        return;
-                    }
+                    // if (blob) {
+                    //     try {
+                    //         const uploadTask = uploadBytesResumable(storageReference, blob);
+                    //         uploadTask.on('state_changed',
+                    //             (snapshot) => {
+                    //                 // Observe state change events such as progress, pause, and resume
+                    //                 // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    //                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    //                 console.log('Upload is ' + progress + '% done');
+                    //                 switch (snapshot.state) {
+                    //                     case 'paused':
+                    //                         console.log('Upload is paused');
+                    //                         break;
+                    //                     case 'running':
+                    //                         console.log('Upload is running');
+                    //                         break;
+                    //                 }
+                    //             },
+                    //             (error) => {
+                    //                 // Handle unsuccessful uploads
+                    //                 console.error('Error uploading image:', error);
+                    //             },
+                    //             () => {
+                    //                 // Handle successful uploads on complete
+                    //                 console.log('Image uploaded successfully');
+                    //             }
+                    //         );
+                    //     } catch (error) {
+                    //         console.error('Error uploading image:', error);
+                    //         return;
+                    //     }
+                    // } else {
+                    //     console.error('No blob available for upload');
+                    //     return;
+                    // }
 
-                    const now = new Date();
-                    const date = now.toISOString().split('T')[0]; // Ngày
-                    const time = now.toTimeString().split(' ')[0]; // Thời gian
+                    // const now = new Date();
+                    // const date = now.toISOString().split('T')[0]; // Ngày
+                    // const time = now.toTimeString().split(' ')[0]; // Thời gian
 
-                    try {
-                        await update(Ref, {
-                            "Price": parseInt(price),
-                            "Name": name,
-                            "Note": note,
-                            "Image": nameImage,
-                            "UpdatedDate": `${date} ${time}`,
-                        });
-                        console.log("Cập nhật thành công!");
-                    } catch (error) {
-                        console.error("Lỗi khi cập nhật:", error);
-                    }
+                    // try {
+                    //     await update(Ref, {
+                    //         "Price": parseInt(price),
+                    //         "Name": name,
+                    //         "Note": note,
+                    //         "Image": nameImage,
+                    //         "UpdatedDate": `${date} ${time}`,
+                    //     });
+                    //     console.log("Cập nhật thành công!");
+                    // } catch (error) {
+                    //     console.error("Lỗi khi cập nhật:", error);
+                    // }
 
-                    navigation.navigate('Admin');
+                    // navigation.navigate('Admin');
 
                 } else {
                     const itemTypePrefix = key.match(/[A-Za-z]+/)[0];
